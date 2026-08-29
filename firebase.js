34515a3d87aaedd318aee0baa7a3bc9778f47675
rishirajnair200-lgtm <rishirajnair200@gmail.com>
2026-08-30 02:14:27 +0530

    Add Firebase initialization script

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCNYfy5Mha_qfICk5R9yjqaOP7G_PhAoDI",
+    authDomain: "berries-cafe-admin.firebaseapp.com",
+    projectId: "berries-cafe-admin",
+    storageBucket: "berries-cafe-admin.firebasestorage.app",
+    messagingSenderId: "934492584472",
+    appId: "1:934492584472:web:b9f6c5969cb3e088d2216d"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
